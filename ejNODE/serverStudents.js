98b24d9ffb373070c3a4ad058ed589faa1c42db8
dmoralesITB <diego.morales.7e8@itb.com>
2026-02-26 11:39:00 +0100

    222

--- ejNODE/serverStudents.js
+++ ejNODE/serverStudents.js
@@ -33,80 +33,42 @@ const server = createServer((req, res) => {
   if (req.method === "GET" && req.url.startsWith("/students/")) {
 
     // 1. Extraer id de la URL
-    const id = req.url.split("/")[2];
     // 2. Buscar alumno en el array
-    const alumno = students.find(s => s.id === id);
     // 3. Si no existe → 404
-    if (!alumno) {
-      return sendJson(res, 404, { message: "Alumno no encontrado" });
-    }
     // 4. Si existe → devolver 200 + alumno
-    return sendJson(res, 200, alumno);
+
   }
 
   // TODO 2: DELETE /students/:id
   if (req.method === "DELETE" && req.url.startsWith("/students/")) {
 
     // 1. Extraer id
-    const id = req.url.split("/")[2];
     // 2. Comprobar si existe
-    const index = students.findIndex(s => s.id === id);
     // 3. Eliminarlo del array
-    students.splice(index, 1);
     // 4. Si no existe → 404
-    if (index === -1) {
-      return sendJson(res, 404, { message: "No se encontró el alumno para borrar" });
-    }
     // 5. Si se elimina → 204 (sin body)
-    res.statusCode = 204;
-    return res.end();
-
 
   }
   // TODO 3: POST /students
   if (req.method === "POST" && req.url === "/students") {
 
     // 1. Leer el body con readBody() --> Es donde esta toda la info del nuevo alumno.
-    readBody(req, (err, newAlumno) => {
-      if (err) return sendJson(res, 400, { message: "Error al leer datos" });
-      // 2. Validar que tenga id, nombre y curso
-      if (!newAlumno.id || !newAlumno.nombre || !newAlumno.curso) {
-        return sendJson(res, 400, { message: "Faltan datos obligatorios" });
-      }
-      // 3. Comprobar que el id no esté repetido
-      const existe = students.some(s => s.id === newAlumno.id);
-      if (existe) {
-        return sendJson(res, 409, { message: "El ID ya existe" });
-      }
-      // 4. Añadir al array students
-      students.push(newAlumno);
-      // 5. Devolver 201 + alumno creado
-      return sendJson(res, 201, newAlumno);
-    });
-    return;
+    // 2. Validar que tenga id, nombre y curso
+    // 3. Comprobar que el id no esté repetido
+    // 4. Añadir al array students
+    // 5. Devolver 201 + alumno creado
+
   }
 
   // TODO 4: PUT /students/:id
   if (req.method === "PUT" && req.url.startsWith("/students/")) {
 
     // 1. Extraer id
-    const id = req.url.split("/")[2];
     // 2. Buscar alumno
-    const alumno = students.find(s => s.id === id);
     // 3. Si no existe → 404
-    if (!alumno) {
-      return sendJson(res, 404, { message: "Alumno no encontrado" });
-    }
     // 4. Leer body con readBody() --> Ahora será otra callback!!!
-    readBody(req, (err, dataActualizada) => {
-      if (err) return sendJson(res, 400, { message: "Error en el JSON" });
-      // 5. Actualizar campos enviados
-      if (dataActualizada.nombre) alumno.nombre = dataActualizada.nombre;
-      if (dataActualizada.curso) alumno.curso = dataActualizada.curso;
-      // 6. Devolver 200 + alumno actualizado
-      return sendJson(res, 200, alumno);
-    });
-    return;
+    // 5. Actualizar campos enviados
+    // 6. Devolver 200 + alumno actualizado
 
   }
 
