0940ffba09f6245305fe8db9aaeb9b1f13555680
dmoralesITB <diego.morales.7e8@itb.com>
2026-02-26 11:55:20 +0100

    ss

--- ejNODE/serverStudentsPromesa.js
+++ ejNODE/serverStudentsPromesa.js
@@ -18,8 +18,8 @@ function sendJson(res, statusCode, data) {
 }
 
 
-
-const server = createServer((req, res) => {
+//asincrono
+const server = createServer(async (req, res) => {
 
   console.log(req.method, req.url);
 
@@ -34,7 +34,6 @@ const server = createServer((req, res) => {
 
     // 1. Extraer id de la URL
     const id = req.url.split('/')[2];
-
     // 2. Buscar alumno en el array
     const student = students.find(s => s.id === id);
 
@@ -45,6 +44,7 @@ const server = createServer((req, res) => {
 
     // 4. Si existe → devolver 200 + alumno
     return sendJson(res, 200, student);
+
   }
 
   // TODO 2: DELETE /students/:id
@@ -68,14 +68,10 @@ const server = createServer((req, res) => {
   }
   // TODO 3: POST /students
   if (req.method === "POST" && req.url === "/students") {
+    try {
+      const alumnoNew = await readBody(req);
+      // 1. Leer el body con readBody() --> await
 
-    // 1. Leer el body con readBody() --> Es donde esta toda la info del nuevo alumno.
-    readBody(req, (err, alumnoNew) => {
-      if (err) {
-        return sendJson(res, 400, { message: "JSON inválido" });
-      }
-
-      // 
       // 2. Validar que tenga id, nombre y curso
       if (!alumnoNew.id || !alumnoNew.nombre || !alumnoNew.curso) {
         return sendJson(res, 400, { message: "Missing required fields: id, nombre, curso" });
@@ -92,7 +88,11 @@ const server = createServer((req, res) => {
 
       // 5. Devolver 201 + alumno creado
       return sendJson(res, 201, alumnoNew);
-    });
+    } catch (err) {
+      //Si hay algún error.
+      return sendJson(res, 400, { message: "JSON inválido" });
+    }
+
   }
 
   // TODO 4: PUT /students/:id
@@ -108,18 +108,20 @@ const server = createServer((req, res) => {
     if (studentIndex === -1) {
       return sendJson(res, 404, { message: `Student with id ${id} not found` });
     }
-    // 4. Leer body con readBody() --> Ahora será otra callback!!!
-    readBody(req, (err, alumnoUpdated) => {
-      if (err) {
-        return sendJson(res, 400, { message: "JSON inválido" });
-      }
+
+    // 4. Leer body con readBody() --> await
+    try {
+      const alumnoUpdated = await readBody(req);
 
       // 5. Actualizar campos enviados
       students[studentIndex] = { ...students[studentIndex], ...alumnoUpdated };
 
       // 6. Devolver 200 + alumno actualizado
       return sendJson(res, 200, students[studentIndex]);
-    });
+    } catch (err) {
+      return sendJson(res, 400, { message: "JSON inválido" });
+    }
+
   }
 
 
@@ -134,7 +136,7 @@ const server = createServer((req, res) => {
 En Node puro, el body no viene empaquetado.
 Llega en trozos.
 Tenemos que montarlo nosotros.*/
-function readBody(req, callback) {
+/*function readBody(req, callback) {
   let body = "";
 
   req.on("data", chunk => {
@@ -150,6 +152,22 @@ function readBody(req, callback) {
     } catch (err) {
       callback(err);
     }
+  });
+}*/
+
+//MEJOR usando PROMESAS
+// Lee el body y devuelve el JSON parseado como Promise (en lugar de callbacks)
+function readBody(req) {
+  return new Promise((resolve, reject) => {
+    let body = "";
+    req.on("data", chunk => body += chunk);
+    req.on("end", () => {
+      try {
+        resolve(JSON.parse(body));
+      } catch (err) {
+        reject(err);
+      }
+    });
   });
 }
 
